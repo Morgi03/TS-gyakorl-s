e75ff7d6e35fe099fc4b2429dfb456513a012c00
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-06 12:31:31 +0200

    first commit

---
+++ src/Book.ts
@@ -0,0 +1,21 @@
+export class Book {
+    #name : string;
+    #rating : number = 1;
+
+    constructor(name: string, rating : number){
+        this.#name = name;
+        this.setRating(rating);
+    }
+
+    public setRating(newrating : number) : void {
+        if(newrating >= 1 && newrating <= 10) {
+            this.#rating = newrating;
+        } else {
+            throw 'Az értékelésnek 1 és 10 közötti számnak kell lennie.';
+        }
+    }
+    toString() : void {
+        console.log("Könyv neve: "+this.#name+", értékelése: "+this.#rating);
+    }
+
+}
